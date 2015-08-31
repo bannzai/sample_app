f4fdef76f7087350f29ed24ec5aebc54ffea03ca
hirose yudai <hirose@na-s.jp>
2015-08-31 14:16:24 +0900

    layout practice finished

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,9 +1,2 @@
+include ApplicationHelper
 
-def full_title(page_title)
-	base_title = "Ruby on Rails Tutorial Sample App"
-	if page_title.empty?
-		base_title
-	else
-		"#{base_title} | #{page_title}"
-	end
-end
