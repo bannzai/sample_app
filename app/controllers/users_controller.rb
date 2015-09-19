eae95b405dbcc82a5a82a119971118d820dbe778
hirose yudai <hirose@na-s.jp>
2015-09-19 13:41:35 +0900

    途中”

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
-  def new
-  end
+	def show 
+		@user = User.find(params[:id])
+	end
+	def new
+		@user = User.new
+	end
+
+	def create 
+		@user = User.new(params[:user])
+		if @user.save
+
+		else
+			render 'new'
+		end
+	end
+
+	private 
+	def user_params
+		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	end
 end
